fa4f1e2c0e783235471647cc333c724f5731c75c
Krishankant89 <toxickush90@gmail.com>
2026-05-06 19:17:20 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     while True:
-        sys.stdout.write("$")
+        sys.stdout.write("$ ")
 
         command = input()
         print(f"{command}: command not found")
